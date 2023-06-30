a417d61699a240e91bd0f14d434310eea67dcc72
Cuongvth <cuongvth191023@gmail.com>
2023-06-30 13:28:43 +0700

    fix nav

--- starter-kit/src/navigation/vertical/index.js
+++ starter-kit/src/navigation/vertical/index.js
@@ -4,24 +4,28 @@ export default [
     to: { name: "index" },
     icon: { icon: "tabler-smart-home" },
   },
+  { heading: "Google Vision" },
   {
-    title: "Nhận dạng trực tiếp",
-    to: { name: "nhan-dang-truc-tiep" },
-    icon: { icon: "tabler-file" },
-  },
-  {
-    title: "Dữ liệu chờ xử lí",
-    to: { name: "du-lieu-cho-xu-li" },
-    icon: { icon: "tabler-file" },
-  },
-  {
-    title: "Căn cước công dân",
-    to: { name: "can-cuoc-cong-dan" },
-    icon: { icon: "tabler-file" },
-  },
-  {
-    title: "Giấy phép lái xe",
-    to: { name: "giay-phep-lai-xe" },
-    icon: { icon: "tabler-file" },
+    title: "Google Vision",
+    icon: { icon: "tabler-smart-home" },
+    children: [
+      {
+        title: "Nhận dạng trực tiếp",
+        to: { name: "nhan-dang-truc-tiep" },
+      },
+      {
+        title: "Dữ liệu chờ xử lí",
+        to: { name: "du-lieu-cho-xu-li" },
+      },
+      {
+        title: "Căn cước công dân",
+        to: { name: "can-cuoc-cong-dan" },
+      },
+      {
+        title: "Giấy phép lái xe",
+        to: { name: "giay-phep-lai-xe" },
+      },
+    ],
+    badgeClass: "bg-primary",
   },
 ];
