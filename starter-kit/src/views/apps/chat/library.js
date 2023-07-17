5929cd91338b65388915abb2e48df75dcdbaa0b7
Cuongvth <cuongvth191023@gmail.com>
2023-07-17 11:47:13 +0700

    backup

--- starter-kit/src/views/apps/chat/library.js
+++ starter-kit/src/views/apps/chat/library.js
@@ -18,41 +18,29 @@ export async function reFormat(cauTraLoi) {
   return new Promise(async (resolve, reject) => {
     cauTraLoi =  cauTraLoi.replace(/\n/g, '<br>');
 
-    const temp = cauTraLoi.split("#");
+    const temp = cauTraLoi.split("<br>");
     var lstDanhTu = [];
 
     for (let index = 0; index < temp.length; index++) {
-      if(index%2 != 0)
+      if(isNaN(temp[index].split(".")[0]))
       {
-        lstDanhTu.push(temp[index]);
-      }     
+        continue;
+      }
+      if(temp[index].includes(":"))
+      {
+        lstDanhTu.push(temp[index].split(":")[0].replace(/^\d+\.\s*/, ''));
+      }  
+      else
+      {
+        lstDanhTu.push(temp[index].replace(/^\d+\.\s*/, ''));
+      }   
     }
-
-    // lstDanhTu.forEach(async element => {
-    //   const link = await searchGoogle(element, 1);
-
-    //   cauTraLoi =  cauTraLoi.replace(`#${element}#`, `<a href="${link.items[0].link}" target="_blank">${element}</a>`);
-    // });
-
-    // resolve(cauTraLoi);
-
-    // await lstDanhTu.forEach(async element => {
-    //   const link = await searchGoogle(element, 1);
-    //   const str = "#"+element+"#";
-
-    //   cauTraLoi = cauTraLoi.replace(
-    //     str,
-    //     `<span class="mixFunction" data="${link}"
-    //     ">${element}</span>`,
-    //   );
-    //   console.log(cauTraLoi);
-    // });
     resolve( await replaceElement(lstDanhTu, cauTraLoi));
   });
 }
 async function replaceElement(arr, cauTraLoi){
   for(var item of arr){
-    cauTraLoi = cauTraLoi.replace(`#${item}#`, `<span style="color:blue" class="mixFunction" onmouseover="this.style.cursor='pointer';">${item}</span>`);
+    cauTraLoi = cauTraLoi.replace(`${item}`, `<span style="color:blue" class="mixFunction" onmouseover="this.style.cursor='pointer';">${item}</span>`);
   }
   
   return cauTraLoi;
